a5938dfbede7b42149a1c7c64ae0d8e2b0f6fec4
Jay Luker <lbjay@reallywow.com>
2015-02-19 16:09:02 -0500

    * remove use of various build-dependent options * allow remote connections from docker default netmask

--- fat/assets/dbinit.sh
+++ fat/assets/dbinit.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e
 
-# source our entrypoint vars
-. /setenv.sh
+sudo -u postgres psql <<< "CREATE USER canvas WITH SUPERUSER;"
+#sudo -u postgres psql <<< "ALTER USER canvas WITH PASSWORD 'canvas';"
+sudo -u postgres psql <<< "CREATE DATABASE canvas_$RAILS_ENV OWNER canvas"
+sudo -u postgres psql <<< "CREATE DATABASE canvas_queue_$RAILS_ENV OWNER canvas"
 
-export CANVAS_LMS_ADMIN_EMAIL=${CANVAS_LMS_ADMIN_EMAIL:-"canvas@example.edu"}
-export CANVAS_LMS_ADMIN_PASSWORD=${CANVAS_LMS_ADMIN_PASSWORD:-"canvas"}
-export CANVAS_LMS_ACCOUNT_NAME=${CANVAS_LMS_ACCOUNT_NAME:-"Canvas Dev"}
-export CANVAS_LMS_STATS_COLLECTION=${CANVAS_LMS_STATS_COLLECTION:-"opt_out"}
-
-if [ ! -d $POSTGRESQL_DATA ]; then
-    mkdir -p $POSTGRESQL_DATA
-    chown -R postgres:postgres $POSTGRESQL_DATA
-    sudo -u postgres /usr/lib/postgresql/$POSTGRES_VERSION/bin/initdb -D $POSTGRESQL_DATA -E 'UTF-8' --lc-collate='en_US.UTF-8' --lc-ctype='en_US.UTF-8'
-    ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem $POSTGRESQL_DATA/server.crt
-    ln -s /etc/ssl/private/ssl-cert-snakeoil.key $POSTGRESQL_DATA/server.key
-fi
-
-sudo -u postgres psql <<< "CREATE USER $DBUSER WITH CREATEDB;"
-sudo -u postgres psql <<< "ALTER USER $DBUSER WITH PASSWORD '$DBPASS';"
+export CANVAS_LMS_ADMIN_EMAIL="canvas@example.edu"
+export CANVAS_LMS_ADMIN_PASSWORD="canvas"
+export CANVAS_LMS_ACCOUNT_NAME="Canvas Dev"
+export CANVAS_LMS_STATS_COLLECTION="opt_out"
 
 cd /opt/canvas-lms \
     && bundle exec rake db:initial_setup
--- fat/assets/setenv.sh
+++ fat/assets/setenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export RAILS_ENV=${RAILS_ENV:-"production"}
 export DBUSER=${DBUSER:-"canvas"}
 export DBPASS=${DBPASS:-"canvas"}
 export POSTGRESQL_DATA=${POSTGRESQL_DATA:-"/var/lib/postgresql/$POSTGRES_VERSION/main"}
+export POSTGRESQL_REMOTE_CONN_NETMASK=${POSTGRESQL_REMOTE_CONN_NETMASK:-"172.17.0.0/16"}
 
 eval "$@"
